a6b11da4ff8abd16d49522d38c6c706b57f24794
lx <1605670114@qq.com>
2020-12-16 17:25:32 +0800

    end 44

---
+++ consumer-hystrix-dashboard-9001/src/main/java/com/lx/microServices/HystrixDashboard/HystrixDashboardApplication.java
@@ -0,0 +1,17 @@
+package com.lx.microServices.HystrixDashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * @author lx
+ * @date 2020/12/16
+ */
+@SpringBootApplication
+@EnableHystrixDashboard
+public class HystrixDashboardApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardApplication.class,args);
+    }
+}
